e7aed214177767de7654a0272a6504167531e289
vressegu <valentin.resseguier@inrae.fr>
2025-02-04 16:49:55 +0100

    config valentin

--- utility/openfoam/perturbUCylinder/wmake.sh
+++ utility/openfoam/perturbUCylinder/wmake.sh
@@ -3,7 +3,7 @@ module use /scratch/mathlab/packages/modules
 module load openfoam/2012
 
 export WM_PROJECT_USER_DIR=$PWD
-export FOAM_USER_APPBIN=/home/gspasov/OpenFoam/platforms/linux64IccDPInt32OptBDW/bin
-export FOAM_USER_LIBBIN=/home/gspasov/OpenFoam/platforms/linux64IccDPInt32OptBDW/lib
+export FOAM_USER_APPBIN=/home/vresseguier/OpenFOAM/vresseguier-v2212/platforms/linux64GccDPInt32Opt/bin
+export FOAM_USER_LIBBIN=/home/vresseguier/OpenFOAM/vresseguier-v2212/platforms/linux64GccDPInt32Opt/lib
 
 wmake -all >& wmake.log &
