25ad07b14e4ddd08ecd1ed9b096bb05487339b04
vressegu <valentin.resseguier@inrae.fr>
2025-02-04 18:56:50 +0100

    update cluster paths for openfoam

--- utility/openfoam/perturbUCylinder/wmake.sh
+++ utility/openfoam/perturbUCylinder/wmake.sh
@@ -1,9 +1,9 @@
 
-module use /scratch/mathlab/packages/modules
-module load openfoam/2012
-
+module load cv-standard
+module load openmpi/psm2/gcc61/3.1.6
+source  /home/ressequierv/Apps/OpenFOAM-v2106/etc/bashrc
 export WM_PROJECT_USER_DIR=$PWD
-export FOAM_USER_APPBIN=/home/vresseguier/OpenFOAM/vresseguier-v2212/platforms/linux64GccDPInt32Opt/bin
-export FOAM_USER_LIBBIN=/home/vresseguier/OpenFOAM/vresseguier-v2212/platforms/linux64GccDPInt32Opt/lib
+export FOAM_USER_APPBIN=/home/ressequierv/Apps/OpenFOAM-v2106/platforms/linux64GccDPInt32Opt/bin
+export FOAM_USER_LIBBIN=/home/ressequierv/Apps/OpenFOAM-v2106/platforms/linux64GccDPInt32Opt/lib
 
 wmake -all >& wmake.log &
