696d02db433f13e95f203e80daed722f3e811e6c
vressegu <valentin.resseguier@inrae.fr>
2025-02-04 16:52:16 +0100

    remove module and sbatch for local runs

--- utility/openfoam/perturbUCylinder/wmake.sh
+++ utility/openfoam/perturbUCylinder/wmake.sh
@@ -1,6 +1,6 @@
 
-module use /scratch/mathlab/packages/modules
-module load openfoam/2012
+#module use /scratch/mathlab/packages/modules
+#module load openfoam/2012
 
 export WM_PROJECT_USER_DIR=$PWD
 export FOAM_USER_APPBIN=/home/vresseguier/OpenFOAM/vresseguier-v2212/platforms/linux64GccDPInt32Opt/bin
